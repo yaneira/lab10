6bf9df922ba61992a61ca78bcef55b4a3d8e9675
yaneira <98625065+yaneira@users.noreply.github.com>
2022-04-21 20:46:15 -0400

    corrected

---
+++ analyze/scripts/script.js
@@ -0,0 +1,12 @@
+/*
+    Student Name:
+    File Name: script.js
+    Date: 
+*/
+
+//Function to display the first picture
+function pic1 {
+    imgSource.src = "images/trunk-bay.jpg"
+    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block
+    figCap.textContent = "Trunk Bay in St. John";
