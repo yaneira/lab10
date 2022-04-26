022f8574f144489af788cd5a56732a31d4cbe934
yaneira <98625065+yaneira@users.noreply.github.com>
2022-04-26 19:29:26 -0400

    corrected

---
+++ apply/script.js
@@ -0,0 +1,13 @@
+/*
+    Student Name:lizeth hernandez
+    File Name: apply/script.js
+    Date: april 26 2022
+*/
+
+//Function to display content
+function content(){
+  var text = document.getElementById("new");
+  text.textContent = "hi. I am a new paragraph that was created with a javaScript function.";
+  text.style.color ="#c0145f";
+  text.style.fontSize = "2em";
+}
